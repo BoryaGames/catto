b7bdb93ab3d7af2e2355d7f740cb333b2b57bcd4
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2022-10-02 19:23:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+Array.prototype.remove = id => {
+  this.splice(id,1);
+};
+String.prototype.json = () => {
+  return JSON.parse(this);
+};
+Object.prototype.str = () => {
+  return JSON.stringify(this);
+};
 module.exports = {
   "server": require("./server.js"),
   "random": require("./random.js"),
