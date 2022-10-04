df3d808f86f5450655a43d400dc1fcefa0364b03
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2022-10-04 14:52:47 +0500

    Update html.js

--- html.js
+++ html.js
@@ -1,5 +1,10 @@
-module.exports = {
+var mod = {
   "disable": text => {
     return text.split("<").join("&lt;").split(">").join("&gt;");
   }
 };
+if (typeof module !== "undefined") {
+  module.exports = mod;
+} else {
+  catto.html = mod;
+}
